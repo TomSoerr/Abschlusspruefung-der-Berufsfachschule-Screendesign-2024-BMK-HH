30b0bc9529bd19a8970cb3795e75055318c4070c
TomSoerr <tomix2004@gmail.com>
2024-04-11 10:53:59 +0200

    Add password link to the website

--- website/main.js
+++ website/main.js
@@ -23,22 +23,32 @@ load js file when for html site
 ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ */
 
 // this is extremely poor. I know that all my files are accessible
-// The reason for this is that no one would thing this is a real website
-// In this case i would need a impressum
+// The reason for this is that no one would think this is a real website
+// In this case i would need a Impressum
 async function generateHash(data) {
   const dataBuffer = new TextEncoder().encode(data);
 
   const hashBuffer = await crypto.subtle.digest('SHA-256', dataBuffer);
 
   const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray.map((byte) => byte.toString(16).padStart(2, '0')).join('');
+  const hashHex = hashArray
+    .map((byte) => byte.toString(16).padStart(2, '0'))
+    .join('');
 
   return hashHex;
 }
 
 const hash = 'bb1965f98f1bc8db46b2d93ff372c68f2d798cb68c93ab2bdfb9d118bcf0ad8a';
 
+// check local storage
 let userInput = localStorage.getItem('passwd');
+
+// check url hash for password
+if (window.location.hash) {
+  if (window.location.hash.match(/#pw=/)) {
+    userInput = window.location.hash.replace('#pw=', '');
+  }
+}
 
 if (userInput === null) {
   userInput = window.prompt('Password eingeben:');
